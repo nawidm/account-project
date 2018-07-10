68aa462e5a4a039afc334955676425988c37117a
nawidm <nawid_m@hotmail.co.uk>
2018-07-10 12:46:19 +0100

    latest

---
+++ src/test/java/qa/com/account/account_application/ServiceTest.java
@@ -0,0 +1,27 @@
+package qa.com.account.account_application;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ServiceTest {
+	
+	Service service;
+	
+	@Test
+	public void testAdd() {
+		service = new Service();
+		Account accountTest = new Account("Nawid", "Mujadidi", 33333333);
+		service.addAccount(accountTest);
+		assertTrue(service.getListOfAccounts().containsValue(accountTest));
+	}
+
+	@Test
+	public void testRemove() {
+		service = new Service();
+		Account accountTest = new Account("Nawid", "Mujadidi", 33333333);
+		service.addAccount(accountTest);
+		service.removeAccount(accountTest);
+		assertTrue(!service.getListOfAccounts().containsValue(accountTest));
+	}
+}
